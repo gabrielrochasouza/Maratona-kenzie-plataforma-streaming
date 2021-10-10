e7def55c1d8bac1b601d43aa9247a36014589ad5
Gabriel13579 <gabrielrs7@outlook.com>
2021-10-10 00:06:13 -0300

    adicionando musicas

--- src/js/baseMusicas.js
+++ src/js/baseMusicas.js
@@ -1,5 +1,83 @@
 const baseMusicas = [
     {
+        'name': 'Highway to Hell',
+        'artist': 'AC DC',
+        'path': './src/audio/musica1.mp3',
+        'album': 'Highway to Hell',
+        'image':'./src/img/album2.jpeg'
+    },
+    {
+        'name': 'Touch too much',
+        'artist': 'AC DC',
+        'path': './src/audio/musica2.mp3',
+        'album': 'Highway to Hell',
+        'image':'./src/img/album3.jpeg'
+    }
+    ,
+    {
+        'name': 'Walk all over you',
+        'artist': 'AC DC',
+        'path': './src/audio/musica3.mp3',
+        'album': 'Highway to Hell',
+        'image':'./src/img/album3.jpeg'
+    }
+    ,
+    {
+        'name': 'Girls got the rythm',
+        'artist': 'AC DC',
+        'path': './src/audio/musica4.mp3',
+        'album': 'Highway to Hell',
+        'image':'./src/img/album3.jpeg'
+    }
+    ,
+    {
+        'name': 'Beating Around The Bush',
+        'artist': 'AC DC',
+        'path': './src/audio/musica5.mp3',
+        'album': 'Highway to Hell',
+        'image':'./src/img/album3.jpeg'
+    }
+    ,
+    {
+        'name': 'Shot Down in Flames',
+        'artist': 'AC DC',
+        'path': './src/audio/musica6.mp3',
+        'album': 'Highway to Hell',
+        'image':'./src/img/album3.jpeg'
+    }
+    ,
+    {
+        'name': 'Get it hot',
+        'artist': 'AC DC',
+        'path': './src/audio/musica7.mp3',
+        'album': 'Highway to Hell',
+        'image':'./src/img/album3.jpeg'
+    }
+    ,
+    {
+        'name': 'If you want blood you got it',
+        'artist': 'AC DC',
+        'path': './src/audio/musica8.mp3',
+        'album': 'Highway to Hell',
+        'image':'./src/img/album3.jpeg'
+    }
+    ,
+    {
+        'name': 'Love Hungry Man',
+        'artist': 'AC DC',
+        'path': './src/audio/musica9.mp3',
+        'album': 'Highway to Hell',
+        'image':'./src/img/album3.jpeg'
+    }
+    ,
+    {
+        'name': 'Night Prowler',
+        'artist': 'AC DC',
+        'path': './src/audio/musica10.mp3',
+        'album': 'Highway to Hell',
+        'image':'./src/img/album3.jpeg'
+    },
+    {
         'name': 'Anonymous Choir - Unus Ex Discipulis Meis',
         'artist': 'Anonymous',
         'path': './src/audio/Anonymous_Choir_-_Unus_Ex_Discipulis_Meis.mp3',
@@ -23,45 +101,32 @@ const baseMusicas = [
     {
         'name': 'Anonymous Choir - Unus Ex Discipulis Meis',
         'artist': 'Anonymous',
-        'path': './src/audio/Anonymous_Choir_-_Unus_Ex_Discipulis_Meis.mp3',
+        'path': './src/audio/Karine_Gilanyan_-_Beethoven_-_Piano_Sonata_nr15_in_D_major_op28_Pastoral_-_I_Allegro.mp3',
         'album': 'Good Enough4',
         'image':'./src/img/album1.jpeg'
     },
     {
         'name': 'Felipe Sarro - C Schumann Scherzo in C minor Op 14',
         'artist': 'Anonymous',
-        'path': './src/audio/Felipe_Sarro_-_11_-_C_Schumann_Scherzo_in_C_minor_Op_14.mp3',
+        'path': './src/audio/Karine_Gilanyan_-_Beethoven_-_Piano_Sonata_nr15_in_D_major_op28_Pastoral_-_II_Andante.mp3',
         'album': 'Good Enough5',
         'image':'./src/img/album2.jpeg'
     },
     {
         'name': 'James Scott - Frog Legs Rag 1906 piano roll',
         'artist': 'Anonymous',
-        'path': './src/audio/James_Scott_-_01_-_Frog_Legs_Rag_1906_piano_roll.mp3',
+        'path': './src/audio/Karine_Gilanyan_-_Beethoven_-_Piano_Sonata_nr15_in_D_major_op28_Pastoral_-_III_Scherzo_Allegro_Vivace.mp3',
         'album': 'Good Enough6',
         'image':'./src/img/album3.jpeg'
     },
     {
         'name': 'Anonymous Choir - Unus Ex Discipulis Meis',
         'artist': 'Anonymous',
-        'path': './src/audio/Anonymous_Choir_-_Unus_Ex_Discipulis_Meis.mp3',
+        'path': './src/audio/Nathan_Eckel_-_03_-_Sonata_in_G_Minor_Didone_Abbandonata_op50_no3_-_III_Allegro_agitato_e_con_disperazione.mp3',
         'album': 'Good Enough7',
         'image':'./src/img/album1.jpeg'
-    },
-    {
-        'name': 'Felipe Sarro - C Schumann Scherzo in C minor Op 14',
-        'artist': 'Anonymous',
-        'path': './src/audio/Felipe_Sarro_-_11_-_C_Schumann_Scherzo_in_C_minor_Op_14.mp3',
-        'album': 'Good Enough8',
-        'image':'./src/img/album2.jpeg'
-    },
-    {
-        'name': 'James Scott - Frog Legs Rag 1906 piano roll',
-        'artist': 'Anonymous',
-        'path': './src/audio/James_Scott_-_01_-_Frog_Legs_Rag_1906_piano_roll.mp3',
-        'album': 'Good Enough9',
-        'image':'./src/img/album3.jpeg'
     }
+
 ];
 
 export {baseMusicas};
